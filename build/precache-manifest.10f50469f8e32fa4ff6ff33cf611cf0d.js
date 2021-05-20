cee4f625d2734fa0698f7c3529ee7d14a13cd6b9
77ba44c230f60e834e5b3c8be10118b2b5c042d1 <amitos80@gmail.com>
2021-05-21 00:46:12 +0300

    whatapp button

---
+++ build/precache-manifest.10f50469f8e32fa4ff6ff33cf611cf0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7fd0d733cb160394a8b4f43753ddb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "025ac80009e3dbc363c2",
+    "url": "/static/css/main.39ade98d.chunk.css"
+  },
+  {
+    "revision": "8f6ffcf268016dd3b6d8",
+    "url": "/static/js/2.72947fed.chunk.js"
+  },
+  {
+    "revision": "025ac80009e3dbc363c2",
+    "url": "/static/js/main.36523e17.chunk.js"
+  },
+  {
+    "revision": "c7d10ede0edd7e2603d6",
+    "url": "/static/js/runtime-main.e03aca7b.js"
+  },
+  {
+    "revision": "9fe23bf225eecfcdb736e063b08a6123",
+    "url": "/static/media/Arimo-Regular.9fe23bf2.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  }
+]);
