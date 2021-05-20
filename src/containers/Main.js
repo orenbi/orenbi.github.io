ad06a05e55b3838476be6595fe06eab436847563
77ba44c230f60e834e5b3c8be10118b2b5c042d1 <amitos80@gmail.com>
2021-05-21 00:45:18 +0300

    whatapp button

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import "./Main.css";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 import Footer from "../components/footer/Footer";
 import Top from "./topbutton/Top";
 import Profile from "./profile/Profile";
+
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab, faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+library.add(fab, faWhatsapp)
+
+const WhatAppWidget = ({}) => (
+    <div className="whatsapp-floating">
+        <a
+            target="_blank"
+            href="https://api.whatsapp.com/send?phone=972544243178&text=פניה מהאתר"
+        >
+            <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+    </div>
+)
 
 export default class Main extends Component {
   render() {
@@ -17,6 +34,8 @@ export default class Main extends Component {
         <Profile />
         <Footer />
         <Top />
+
+        <WhatAppWidget/>
       </div>
     );
   }
